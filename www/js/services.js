383d2a7510b18fdbe0747a17c2e08c54585c8324
nicolaiduus <nhwduus@hotmail.com>
2015-10-07 13:31:41 +0200

    Added js-data to the project

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,41 @@
 angular.module('starter.services', [])
 
+  .factory('Data',function(DS){
+    DS.registerAdapter('localforage',new DSLocalForageAdapter(),{default:true});
+    return DS.defineResource('data');
+  })
+
+  .factory('DataService', function (Data) {
+
+    var dataservice = {};
+
+    Data.inject([
+      {
+        /*TODO - Inject testdata*/
+      }
+    ]);
+
+    dataservice.createNewData = function () {
+      var id = '_' + Math.random().toString(36).substr(2, 9);
+
+      Data.create({id: id,/*TODO - Create new dataobject*/});
+        DS.digest();
+      };
+
+    dataservice.deleteData = function (id) {
+      /*TODO - delete dataobject*/
+    };
+
+    dataservice.getAllData = function () {
+      return Data.getAll();
+    };
+
+    dataservice.getData = function (id) {
+      return Data.get(id);
+    };
+
+    return dataservice;
+  })
 
   .factory('questionState', function($timeout) {
     var obj = {
@@ -24,14 +60,14 @@ angular.module('starter.services', [])
       ];
 
       return monthNames[obj.selectedDate.getMonth()];
-    }
+    };
 
     obj.getSelectedDayOfTheWeek = function() {
       var dayOfWeekNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
       ];
 
       return dayOfWeekNames[obj.selectedDate.getDay()];
-    }
+    };
 
     return obj;
   });
