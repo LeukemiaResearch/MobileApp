9554c66050290d4f6d6fb50e52e426287bfd96ff
Morten Mathiasen <morten@tranebaerkaeret.dk>
2015-11-11 10:41:34 +0100

    Styling of start and end time stamps

--- www/js/dataoverview_controller.js
+++ www/js/dataoverview_controller.js
@@ -2,6 +2,120 @@ angular.module('starter.controllers')
 
   .controller('dataoverviewController', function ($scope, $filter, questionState, dataProvider) {
 
+    /* Time and date picker */
+    $scope.updateStartTimeStamp = function() {
+
+    };
+    if ($scope.startTimePickerObject === undefined)
+      $scope.startTimePickerObject = {
+        displayValue: function () {
+          var selectedTime = new Date($scope.startTimePickerObject.inputEpochTime * 1000);
+          return selectedTime.getUTCHours().toLocaleString('en', {minimumIntegerDigits: 2, useGrouping: false})
+            + ' : '
+            + selectedTime.getUTCMinutes().toLocaleString('en', {minimumIntegerDigits: 2, useGrouping: false});
+        },
+        inputEpochTime: ((questionState.timeStamp?questionState.timeStamp:new Date()).getHours() * 60 * 60 +
+        Math.floor((questionState.timeStamp?questionState.timeStamp:new Date()).getMinutes() / 5) * 5 * 60),  //Optional
+        step: 5,  //Optional
+        format: 24,  //Optional
+        titleLabel: 'Tidspunkt',  //Optional
+        setLabel: 'Vælg',  //Optional
+        closeLabel: 'Anuller',  //Optional
+        setButtonType: 'button-positive',  //Optional
+        closeButtonType: 'button-stable',  //Optional
+        callback: function (val) {    //Mandatory
+          if (val) {
+            $scope.startTimePickerObject.inputEpochTime = val;
+            $scope.updateStartTimeStamp();
+          }
+        }
+      };
+    if ($scope.startDatepickerObject === undefined)
+      $scope.startDatepickerObject = {
+        titleLabel: 'Dato',  //Optional
+        todayLabel: 'I dag',  //Optional
+        closeLabel: 'Luk',  //Optional
+        setLabel: 'Vælg',  //Optional
+        setButtonType: 'button-assertive',  //Optional
+        todayButtonType: 'button-assertive',  //Optional
+        closeButtonType: 'button-assertive',  //Optional
+        inputDate: (questionState.timeStamp?questionState.timeStamp:new Date()),  //Optional
+        mondayFirst: true,  //Optional
+        //disabledDates: disabledDates, //Optional
+        weekDaysList: ["Sø", "Ma", "Ti", "On", "To", "Fr", "Lø"], //Optional
+        monthList: ["Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"], //Optional
+        templateType: 'popup', //Optional
+        showTodayButton: 'true', //Optional
+        modalHeaderColor: 'bar-positive', //Optional
+        modalFooterColor: 'bar-positive', //Optional
+        //from: new Date(2012, 8, 2), //Optional
+        //to: new Date(2018, 8, 25),  //Optional
+        callback: function (val) {  //Mandatory
+          if (val) {
+            $scope.startDatepickerObject.inputDate = val;
+            $scope.updateStartTimeStamp();
+          }
+        },
+        dateFormat: 'dd-MM-yyyy', //Optional
+        closeOnSelect: false, //Optional
+      };
+    $scope.updateEndTimeStamp = function() {
+
+    };
+    if ($scope.endTimePickerObject === undefined)
+      $scope.endTimePickerObject = {
+        displayValue: function () {
+          var selectedTime = new Date($scope.endTimePickerObject.inputEpochTime * 1000);
+          return selectedTime.getUTCHours().toLocaleString('en', {minimumIntegerDigits: 2, useGrouping: false})
+            + ' : '
+            + selectedTime.getUTCMinutes().toLocaleString('en', {minimumIntegerDigits: 2, useGrouping: false});
+        },
+        inputEpochTime: ((questionState.timeStamp?questionState.timeStamp:new Date()).getHours() * 60 * 60 +
+        Math.floor((questionState.timeStamp?questionState.timeStamp:new Date()).getMinutes() / 5) * 5 * 60),  //Optional
+        step: 5,  //Optional
+        format: 24,  //Optional
+        titleLabel: 'Tidspunkt',  //Optional
+        setLabel: 'Vælg',  //Optional
+        closeLabel: 'Anuller',  //Optional
+        setButtonType: 'button-positive',  //Optional
+        closeButtonType: 'button-stable',  //Optional
+        callback: function (val) {    //Mandatory
+          if (val) {
+            $scope.endTimePickerObject.inputEpochTime = val;
+            $scope.updateEndTimeStamp();
+          }
+        }
+      };
+    if ($scope.endDatepickerObject === undefined)
+      $scope.endDatepickerObject = {
+        titleLabel: 'Dato',  //Optional
+        todayLabel: 'I dag',  //Optional
+        closeLabel: 'Luk',  //Optional
+        setLabel: 'Vælg',  //Optional
+        setButtonType: 'button-assertive',  //Optional
+        todayButtonType: 'button-assertive',  //Optional
+        closeButtonType: 'button-assertive',  //Optional
+        inputDate: (questionState.timeStamp?questionState.timeStamp:new Date()),  //Optional
+        mondayFirst: true,  //Optional
+        //disabledDates: disabledDates, //Optional
+        weekDaysList: ["Sø", "Ma", "Ti", "On", "To", "Fr", "Lø"], //Optional
+        monthList: ["Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"], //Optional
+        templateType: 'popup', //Optional
+        showTodayButton: 'true', //Optional
+        modalHeaderColor: 'bar-positive', //Optional
+        modalFooterColor: 'bar-positive', //Optional
+        //from: new Date(2012, 8, 2), //Optional
+        //to: new Date(2018, 8, 25),  //Optional
+        callback: function (val) {  //Mandatory
+          if (val) {
+            $scope.endDatepickerObject.inputDate = val;
+            $scope.updateEndTimeStamp();
+          }
+        },
+        dateFormat: 'dd-MM-yyyy', //Optional
+        closeOnSelect: false, //Optional
+      };
+
     /* Inspired by Lee Byron's test data generator. */
     function stream_layers(n, m, o) {
       if (arguments.length < 3) o = 0;
@@ -169,7 +283,7 @@ angular.module('starter.controllers')
 
     $scope.changeDisplayType = function (dis) {
       $scope.displaytype = dis;
-      console.log("Type:" + dis);
+      //console.log("Type:" + dis);
       if (dis == "chart") {
         $scope.chartbuttonclass = "button-dark";
         $scope.tablebuttonclass = "button-light";
