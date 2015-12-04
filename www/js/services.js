43f3b87f7a9ce41593ac7e26785a4a78663c390e
Roland Tolnay <roland.ty9@gmail.com>
2015-12-04 21:38:49 +0100

    commit before pull

--- www/js/services.js
+++ www/js/services.js
@@ -1,34 +1,34 @@
 angular.module('starter.services', [])
 
-  .factory('DataStore', function () {
+  .factory('DataStore',function(){
     var store = new JSData.DS();
-    store.registerAdapter('localforage', new DSLocalForageAdapter(), {default: true});
+    store.registerAdapter('localforage',new DSLocalForageAdapter(),{default:true});
     return store;
   })
 
-  .factory('DailyData', function (DataStore) {
+  .factory('DailyData',function(DataStore){
     return DataStore.defineResource('daily');
   })
 
-  .factory('PainData', function (DataStore) {
+  .factory('PainData',function(DataStore){
     return DataStore.defineResource('pain');
   })
 
-  .factory('MedicineData', function (DataStore) {
+  .factory('MedicineData',function(DataStore){
     return DataStore.defineResource('medicine');
   })
 
-  .factory('BloodsampleData', function (DataStore) {
+  .factory('BloodsampleData',function(DataStore){
     return DataStore.defineResource('bloodsample');
   })
 
-  .factory('MucositisData', function (DataStore) {
+  .factory('MucositisData',function(DataStore){
     return DataStore.defineResource('mucositis');
   })
 
-  .factory('IdGenerator', function () {
+  .factory('IdGenerator',function(){
     var id = {};
-    id.generateId = function () {
+    id.generateId = function(){
       return '_' + Math.random().toString(36).substr(2, 9);
     };
     return id;
@@ -65,11 +65,11 @@ angular.module('starter.services', [])
       return DailyData.get(id);
     };
 
-    dailydataservice.getDailyDataOnDate = function (date) {
+    dailydataservice.getDailyDataOnDate = function(date){
       return DailyData.filter({
         where: {
           date: {
-            '==': date.setHours(0, 0, 0, 0)
+            '==': date.setHours(0,0,0,0)
           }
         }
       });
@@ -82,30 +82,21 @@ angular.module('starter.services', [])
     return dailydataservice;
   })
 
-  .factory('PainDataService', function (IdGenerator, PainData, questionState) {
+  .factory('PainDataService', function(IdGenerator, PainData, questionState){
 
     var paindataservice = {};
 
     //Inject TestData
     /*PainData.inject([{id: 1, date: new Date(2015, 8, 27, 10, 0, 0, 0), painType:'stomach', painScore: 6, morphine:false, morphineType:'', morphineDose:'', morphineMeasureUnit:''},
-     {id: 2, date: new Date(2015, 8, 27, 17, 23, 22, 0), painType:'stomach', painScore: 8, morphine:true, morphineType:'oral', morphineDose:10.0, morphineMeasureUnit:'mg/dag'},
-     {id: 3, date: new Date(2015, 9, 16, 19, 7, 34, 0), painType:'stomach', painScore: 2, morphine:false, morphineType:'', morphineDose:'', morphineMeasureUnit:''},
-     {id: 4, date: new Date(2015, 10, 1, 18, 53, 17, 0), painType:'stomach', painScore: 4, morphine:false, morphineType:'', morphineDose:'', morphineMeasureUnit:''},
-     {id: 5, date: new Date(2015, 10, 1, 22, 18, 56, 0), painType:'stomach', painScore: 8, morphine:true, morphineType:'oral', morphineDose:7.5, morphineMeasureUnit:'mg/dag'}]);*/
+      {id: 2, date: new Date(2015, 8, 27, 17, 23, 22, 0), painType:'stomach', painScore: 8, morphine:true, morphineType:'oral', morphineDose:10.0, morphineMeasureUnit:'mg/dag'},
+      {id: 3, date: new Date(2015, 9, 16, 19, 7, 34, 0), painType:'stomach', painScore: 2, morphine:false, morphineType:'', morphineDose:'', morphineMeasureUnit:''},
+      {id: 4, date: new Date(2015, 10, 1, 18, 53, 17, 0), painType:'stomach', painScore: 4, morphine:false, morphineType:'', morphineDose:'', morphineMeasureUnit:''},
+      {id: 5, date: new Date(2015, 10, 1, 22, 18, 56, 0), painType:'stomach', painScore: 8, morphine:true, morphineType:'oral', morphineDose:7.5, morphineMeasureUnit:'mg/dag'}]);*/
 
     //Pain data functions
-    paindataservice.createPainData = function (date, painType, painScore, morphine, morphineType, morphineDose, morphineMeasureUnit) {
+    paindataservice.createPainData = function (date, painType, painScore, morphine, morphineType, morphineDose, morphineMeasureUnit){
       var id = IdGenerator.generateId();
-      var obj = PainData.createInstance({
-        id: id,
-        timeStamp: date,
-        painType: painType,
-        painScore: painScore,
-        morphine: morphine,
-        morphineType: morphineType,
-        morphineDose: morphineDose,
-        morphineMeasureUnit: morphineMeasureUnit
-      });
+      var obj = PainData.createInstance({id: id, timeStamp: date, painType: painType, painScore: painScore, morphine: morphine, morphineType: morphineType, morphineDose: morphineDose, morphineMeasureUnit: morphineMeasureUnit});
       PainData.inject(obj);
       return obj;
     };
@@ -136,13 +127,13 @@ angular.module('starter.services', [])
       return PainData.get(id);
     };
 
-    paindataservice.getPainDataOnDate = function (date) {
-      var temp = date.setHours(0, 0, 0, 0);
+    paindataservice.getPainDataOnDate = function(date){
+      var temp = date.setHours(0,0,0,0);
       return PainData.filter({
         where: {
           timeStamp: {
             '>': temp,
-            '<': temp.setDate(temp.getDate() + 1)
+            '<': temp.setDate(temp.getDate()+1)
           }
         }
       });
@@ -152,18 +143,18 @@ angular.module('starter.services', [])
       PainData.destroy(id);
     };
 
-    paindataservice.finishedStep = function (stepNumber) {
-      if (stepNumber == 1) {
+    paindataservice.finishedStep = function(stepNumber) {
+      if (stepNumber==1) {
         return questionState.timeStamp !== undefined;
       }
-      else if (stepNumber == 2) {
+      else if (stepNumber==2) {
         return true;
       }
-      else if (stepNumber == 3) {
+      else if (stepNumber==3) {
         return questionState.painType !== undefined;
       }
-      else if (stepNumber == 4) {
-        return (questionState.flaccvalue !== undefined && questionState.flaccvalue[0] !== undefined && questionState.flaccvalue[1] !== undefined && questionState.flaccvalue[2] !== undefined) || questionState.selectedSmiley !== undefined;
+      else if (stepNumber==4) {
+        return (questionState.flaccvalue !== undefined && questionState.flaccvalue[0] !== undefined && questionState.flaccvalue[1] !== undefined && questionState.flaccvalue[2] !== undefined) || questionState.selectedSmiley !==undefined;
       }
       else
         return false;
@@ -174,14 +165,14 @@ angular.module('starter.services', [])
     return paindataservice
   })
 
-  .factory('MedicineDataService', function (IdGenerator, MedicineData, questionState) {
+  .factory('MedicineDataService', function(IdGenerator, MedicineData, questionState){
 
     var medicinedataservice = {};
 
     //MedicineData
     //MedicineData.inject([{id: 1, date: new Date(2015, 8, 27, 0, 0, 0, 0), sixmp:0, mtx:0},{id: 2, date: new Date(2015, 9, 2, 0, 0, 0, 0), sixmp:0, mtx:0},{id: 3, date: new Date(2015, 9, 16, 0, 0, 0, 0), sixmp:10.0, mtx:10.0}, {id: 4, date: new Date(2015, 9, 17, 0, 0, 0, 0), sixmp:0, mtx:0}, {id: 5, date: new Date(2015, 10, 1, 0, 0, 0, 0), sixmp:0, mtx:0},{id: 6, date: new Date(2015, 10, 7, 0, 0, 0, 0), sixmp:25.0, mtx:10.0}]);
 
-    medicinedataservice.createMedicineData = function (date, sixmp, mtx) {
+    medicinedataservice.createMedicineData = function (date, sixmp, mtx){
       var id = IdGenerator.generateId();
       var obj = MedicineData.createInstance({id: id, timeStamp: date, sixmp: sixmp, mtx: mtx});
       MedicineData.inject(obj);
@@ -207,13 +198,13 @@ angular.module('starter.services', [])
       return MedicineData.get(id);
     };
 
-    medicinedataservice.getMedicineDataOnDate = function (date) {
-      var temp = date.setHours(0, 0, 0, 0);
+    medicinedataservice.getMedicineDataOnDate = function(date){
+      var temp = date.setHours(0,0,0,0);
       return MedicineData.filter({
         where: {
           timeStamp: {
             '>': temp,
-            '<': temp.setDate(temp.getDate() + 1)
+            '<': temp.setDate(temp.getDate()+1)
           }
         }
       });
@@ -223,11 +214,11 @@ angular.module('starter.services', [])
       MedicineData.destroy(id);
     };
 
-    medicinedataservice.finishedStep = function (stepNumber) {
-      if (stepNumber == 1) {
+    medicinedataservice.finishedStep = function(stepNumber) {
+      if (stepNumber==1){
         return questionState.timeStamp !== undefined;
       }
-      else if (stepNumber == 2) {
+      else if (stepNumber==2){
         return (questionState.SixMP !== undefined) && (questionState.MTX !== undefined);
       }
       else
@@ -239,26 +230,17 @@ angular.module('starter.services', [])
     return medicinedataservice;
   })
 
-  .factory('BloodsampleDataService', function (IdGenerator, BloodsampleData, questionState) {
+  .factory('BloodsampleDataService', function(IdGenerator, BloodsampleData, questionState){
 
     var bloodsampledataservice = {};
 
     //BloodsampleData
     /*BloodsampleData.inject([{id: 1, date: new Date(2015, 9, 2, 15, 15, 0, 0), leucocytes: 4.5, neutrofile: 7.8, thrombocytes: 45.2, hemoglobin: 3.7, alat: 3465, crp: 453},
-     {id: 2, date: new Date(2015, 10, 7, 12, 2, 34, 0), leucocytes:78.5, neutrofile:12.3, thrombocytes:15.0, hemoglobin:4.1, alat:2635, crp:251}]);*/
+      {id: 2, date: new Date(2015, 10, 7, 12, 2, 34, 0), leucocytes:78.5, neutrofile:12.3, thrombocytes:15.0, hemoglobin:4.1, alat:2635, crp:251}]);*/
 
-    bloodsampledataservice.createBloodsampleData = function (date, leucocytes, neutrofile, thrombocytes, hemoglobin, alat, crp) {
+    bloodsampledataservice.createBloodsampleData = function (date, leucocytes, neutrofile, thrombocytes, hemoglobin, alat, crp){
       var id = IdGenerator.generateId();
-      var obj = BloodsampleData.createInstance({
-        id: id,
-        timeStamp: date,
-        leucocytes: leucocytes,
-        neutrofile: neutrofile,
-        thrombocytes: thrombocytes,
-        hemoglobin: hemoglobin,
-        alat: alat,
-        crp: crp
-      });
+      var obj = BloodsampleData.createInstance({id: id, timeStamp:date, leucocytes: leucocytes,neutrofile: neutrofile,thrombocytes: thrombocytes,hemoglobin: hemoglobin,alat: alat,crp: crp});
       BloodsampleData.inject(obj);
       console.log("ID: " + obj.id + "Leuko: " + obj.leucocytes);
       return obj;
@@ -283,13 +265,13 @@ angular.module('starter.services', [])
       return BloodsampleData.get(id);
     };
 
-    bloodsampledataservice.getBloodsampleDataOnDate = function (date) {
-      var temp = date.setHours(0, 0, 0, 0);
+    bloodsampledataservice.getBloodsampleDataOnDate = function(date){
+      var temp = date.setHours(0,0,0,0);
       return BloodsampleData.filter({
         where: {
           timeStamp: {
             '>': temp,
-            '<': temp.setDate(temp.getDate() + 1)
+            '<': temp.setDate(temp.getDate()+1)
           }
         }
       });
@@ -299,17 +281,17 @@ angular.module('starter.services', [])
       BloodsampleData.destroy(id);
     };
 
-    bloodsampledataservice.finishedStep = function (stepNumber) {
-      if (stepNumber == 1) {
+    bloodsampledataservice.finishedStep = function(stepNumber) {
+      if (stepNumber==1){
         return questionState.timeStamp !== undefined;
       }
-      else if (stepNumber == 2) {
-        return (questionState.Leukocytter !== undefined && 0.0 <= parseFloat(questionState.Leukocytter) && parseFloat(questionState.Leukocytter) <= 100.0) &&
-          (questionState.Neutrofile !== undefined && 0.0 <= parseFloat(questionState.Neutrofile) && parseFloat(questionState.Neutrofile) <= 20.0) &&
-          (questionState.Thombocytter !== undefined && 0.0 <= parseFloat(questionState.Thombocytter) && parseFloat(questionState.Thombocytter) <= 100.0) &&
-          (questionState.Hemoglobin !== undefined && 2.0 <= parseFloat(questionState.Hemoglobin) && parseFloat(questionState.Hemoglobin) <= 10.0) &&
-          (questionState.Alat !== undefined && 99 <= parseInt(questionState.Alat) && parseInt(questionState.Alat) <= 9999) &&
-          (questionState.CRP !== undefined && 1 <= parseInt(questionState.CRP) && parseInt(questionState.CRP) <= 999);
+      else if (stepNumber==2){
+        return (questionState.Leukocytter !== undefined && 0.0<=parseFloat(questionState.Leukocytter) && parseFloat(questionState.Leukocytter)<=100.0) &&
+        (questionState.Neutrofile !== undefined && 0.0<=parseFloat(questionState.Neutrofile) && parseFloat(questionState.Neutrofile)<=20.0) &&
+        (questionState.Thombocytter !== undefined && 0.0<=parseFloat(questionState.Thombocytter) && parseFloat(questionState.Thombocytter)<=100.0) &&
+        (questionState.Hemoglobin !== undefined && 2.0<=parseFloat(questionState.Hemoglobin) && parseFloat(questionState.Hemoglobin)<=10.0) &&
+        (questionState.Alat !== undefined && 99<=parseInt(questionState.Alat) && parseInt(questionState.Alat)<=9999) &&
+        (questionState.CRP !== undefined && 1<=parseInt(questionState.CRP) && parseInt(questionState.CRP)<=999);
       }
       else
         return false;
@@ -320,7 +302,7 @@ angular.module('starter.services', [])
     return bloodsampledataservice;
   })
 
-  .factory('MucositisDataService', function (IdGenerator, MucositisData, questionState) {
+  .factory('MucositisDataService', function(IdGenerator, MucositisData, questionState){
 
     var mucositisdataservice = {};
 
@@ -329,16 +311,9 @@ angular.module('starter.services', [])
     //MucositisData
     //MucositisData.inject([{id: 1, date: new Date(2015, 9, 16, 19, 14, 22, 0), mucositisScore:7, nauseaScore:6},{id: 2, date: new Date(2015, 10, 7, 19, 46, 0, 0), mucositisScore:9, nauseaScore:8}]);
 
-    mucositisdataservice.createMucositisData = function (timeStamp, pain, ulcers, food, nauseaScore) {
+    mucositisdataservice.createMucositisData = function (timeStamp, pain, ulcers, food, nauseaScore){
       var id = IdGenerator.generateId();
-      var obj = MucositisData.createInstance({
-        id: id,
-        timeStamp: timeStamp,
-        pain: pain,
-        ulcers: ulcers,
-        food: food,
-        'nauseaScore': nauseaScore
-      });
+      var obj = MucositisData.createInstance({id: id, timeStamp: timeStamp, pain: pain, ulcers: ulcers, food:food, 'nauseaScore': nauseaScore});
       MucositisData.inject(obj);
       latestId = id;
       return obj;
@@ -356,7 +331,7 @@ angular.module('starter.services', [])
     };
 
 
-    mucositisdataservice.getAllMucositisData = function () {
+     mucositisdataservice.getAllMucositisData = function () {
       return MucositisData.getAll();
     };
 
@@ -364,13 +339,13 @@ angular.module('starter.services', [])
       return MucositisData.get(id);
     };
 
-    mucositisdataservice.getMucositisDataOnDate = function (date) {
-      var temp = date.setHours(0, 0, 0, 0);
+    mucositisdataservice.getMucositisDataOnDate = function(date){
+      var temp = date.setHours(0,0,0,0);
       return MucositisData.filter({
         where: {
           timeStamp: {
             '>': temp,
-            '<': temp.setDate(temp.getDate() + 1)
+            '<': temp.setDate(temp.getDate()+1)
           }
         }
       });
@@ -380,12 +355,12 @@ angular.module('starter.services', [])
       MucositisData.destroy(id);
     };
 
-    mucositisdataservice.finishedStep = function (stepNumber) {
-      if (stepNumber == 1)
+    mucositisdataservice.finishedStep = function(stepNumber) {
+      if (stepNumber==1)
         return questionState.timeStamp !== undefined;
-      else if (stepNumber == 2)
+      else if (stepNumber==2)
         return questionState.groupvalue !== undefined && questionState.groupvalue[0] !== undefined && questionState.groupvalue[1] !== undefined && questionState.groupvalue[2] !== undefined;
-      else if (stepNumber == 3)
+      else if (stepNumber==3)
         return questionState.nauseaScore;
       else
         return false;
@@ -467,13 +442,13 @@ angular.module('starter.services', [])
     return mucositisdataservice;
   })
 
-  .factory('questionState', function () {
+  .factory('questionState', function() {
     return {
       type: undefined //e.g. pain fo9r pain questions
     };
   })
 
-  .factory('calendarFactory', function (DailyDataService) {
+  .factory('calendarFactory', function(DailyDataService) {
 
     var obj = {};
 
@@ -491,30 +466,32 @@ angular.module('starter.services', [])
       return monthNames[obj.selectedDate.getMonth()];
     };
 
-    obj.getSelectedDayOfTheWeek = function () {
+    obj.getSelectedDayOfTheWeek = function() {
       var dayOfWeekNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
       ];
 
       return dayOfWeekNames[obj.selectedDate.getDay()];
     };
 
-    obj.events = [];
+    obj.events = [
+    ];
 
-    obj.addEvent = function (foo) {
+    obj.addEvent = function(foo) {
       obj.events.push({foo: foo, date: obj.selectedDate});
     };
 
-    obj.removeEvent = function (foo) {
+    obj.removeEvent = function(foo) {
       var index = -1;
-      for (var i = 0; i < obj.events.length; ++i) {
+      for (var i=0; i<obj.events.length; ++i)
+      {
         if (obj.events[i]['date'].getTime() == obj.selectedDate.getTime()
-          && obj.events[i]['foo'] == foo) {
+        && obj.events[i]['foo'] == foo) {
           index = i;
           break;
         }
       }
       if (index > -1)
-        obj.events.splice(index, 1);
+        obj.events.splice(index,1);
 
     };
 
